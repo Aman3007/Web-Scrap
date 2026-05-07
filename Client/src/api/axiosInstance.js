ae57a479cae25d7dd360bf245f131010c536f351
Aman <amanchauhan30072002@gmail.com>
2026-05-07 21:05:38 +0530

    Refactor: Update API base URL to production endpoint

--- Client/src/api/axiosInstance.js
+++ Client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://web-scrap-backend-w1d1.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
   withCredentials: true,
