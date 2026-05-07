23deef3efb659081189c0295954d59e6b765bd20
Aman <amanchauhan30072002@gmail.com>
2026-05-07 21:16:27 +0530

    Refactor: Update cookie options for secure and sameSite settings

--- Server/src/controllers/authController.js
+++ Server/src/controllers/authController.js
@@ -4,8 +4,8 @@ import { asyncHandler, successResponse } from '../utils/helpers.js';
 
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'strict',
+  secure: true, 
+  sameSite: 'none', 
   maxAge: 7 * 24 * 60 * 60 * 1000, 
 };
 
